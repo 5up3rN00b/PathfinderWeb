c556b8b78b092e9b10cad36fcc16f18f11095f0e
5up3rN00b <aaron.linear@gmail.com>
2020-11-07 14:02:09 -0800

    Added register and login

--- html/register.php
+++ html/register.php
@@ -1,1 +1,19 @@
 <?php
+//require '../templates/helper.php';
+
+$url = "";
+$data = array(
+    'origins' => array(
+        array(
+            'latitude' => 37,
+            'longitude' => -122
+        ),
+        array(
+            'latitude' => 32,
+            'longitude' => -117
+        )
+    ),
+    'travelMode' => 'driving'
+);
+
+echo json_encode($data);
