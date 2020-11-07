dbb50dbadbd4630faf747e89b549d1a7cab0862d
5up3rN00b <aaron.linear@gmail.com>
2020-11-07 14:19:32 -0800

    Changed back to resourcesets

--- html/register.php
+++ html/register.php
@@ -29,7 +29,7 @@ $options = array(
 $context = stream_context_create($options);
 $result = json_decode(file_get_contents($url, false, $context));
 
-var_dump($result);
+//var_dump($result);
 //print_r($result->{'results'});
 //print_r($result['results']);
-print_r($result->results);
+print_r($result->resourceSets);
