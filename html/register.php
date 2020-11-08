a29b3be56135c682cacf61bcf962284f656b8e13
5up3rN00b <aaron.linear@gmail.com>
2020-11-08 11:02:00 -0800

    Added some testing for register

--- html/register.php
+++ html/register.php
@@ -4,6 +4,8 @@ require '../templates/helper.php';
 if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['org'])) {
     $hashed = hash('sha256', $_POST['password']);
 
+    echo "Hello world";
+
     $sth = $db->prepare("SELECT * FROM `users` WHERE `username`=? OR `email`=?");
     $sth->execute([$_POST['email'], $hashed]);
     $user = $sth->fetchAll();
@@ -17,3 +19,5 @@ if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['org']))
         echo "Cannot create user";
     }
 }
+
+echo "Hello";
