bbd84ee9cfcb1cfc700db5ab23020bbeb035b135
5up3rN00b <aaron.linear@gmail.com>
2020-11-08 10:04:27 -0800

    Added login and register

--- html/login.php
+++ html/login.php
@@ -1,2 +1,16 @@
 <?php
 require '../templates/helper.php';
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $hashed = hash('sha256', $_POST['password']);
+
+    $sth = $db->prepare("SELECT * FROM users WHERE `email`=? AND `password`=?");
+    $sth->execute([$_POST['email'], $hashed]);
+    $user = $sth->fetchAll();
+
+    if (empty($user)) {
+        echo "Not logged in";
+    } else {
+        echo "Logged in";
+    }
+}
--- html/register.php
+++ html/register.php
@@ -1,2 +1,19 @@
 <?php
 require '../templates/helper.php';
+
+if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['org'])) {
+    $hashed = hash('sha256', $_POST['password']);
+
+    $sth = $db->prepare("SELECT * FROM `users` WHERE `username`=? OR `email`=?");
+    $sth->execute([$_POST['email'], $hashed]);
+    $user = $sth->fetchAll();
+
+    if (empty($sth)) {
+        $sth = $db->prepare("INSERT INTO `users` (`email`, `password`, `org`) VALUES (?, ?, ?)");
+        $sth->execute([$_POST['email'], $hashed, $_POST['org']]);
+
+        echo "Created user";
+    } else {
+        echo "Cannot create user";
+    }
+}
